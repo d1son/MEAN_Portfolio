0e1fd0d7a97f018fe7c9faf8a8937e06f259229e
d1son <dson120@gmail.com>
2016-03-26 23:10:58 -0400

    set up some controllers, set up the module, the original message to show up using angular

---
+++ public/app.js
@@ -0,0 +1,40 @@
+//create angular module
+	//added ngRoute for all routing purposes
+var myPort = angular.module('myPort', ['ngRoute']);
+
+
+// configure routes
+myPort.config(function($routeProvider) {
+	$routeProvider
+
+	//route for home page, this is controlled by the main controller
+	.when('/', {
+		templateUrl : './pages/home.html',
+		controller : 'mainController'
+	})
+
+	//route for about page, this has its own controller
+	.when('/about', {
+		templateUrl : './pages/about.html',
+		controller : 'aboutController'
+	})
+
+	//route for contact page, this has its own controller
+	.when('/contact', {
+		templateUrl : './pages/contact.html',
+		controller : 'contactController'
+	})
+});
+
+//created main controller and added the scope of the controller
+myPort.controller('mainController', function($scope){
+	$scope.message = "This is the main page!!!"
+});
+
+myPort.controller('aboutController', function($scope){
+	$scope.message = "This is the about me page!!"
+});
+
+myPort.controller('contactController', function($scope){
+	$scope.message = "This is the contact me page!!"
+});
