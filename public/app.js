4c8017777103217338d7272b9b75b4160807aef1
d1son <dson120@gmail.com>
2016-03-28 15:24:34 -0400

    got the angular routes to map to home, about, and contact links

--- public/app.js
+++ public/app.js
@@ -4,37 +4,47 @@ var myPort = angular.module('myPort', ['ngRoute']);
 
 
 // configure routes
-myPort.config(function($routeProvider) {
+myPort.config(function($routeProvider, $locationProvider) {
 	$routeProvider
 
 	//route for home page, this is controlled by the main controller
 	.when('/', {
-		templateUrl : './pages/home.html',
+		templateUrl : '/pages/home.html',
+		controller : 'mainController'
+	})
+
+	.when('/home', {
+		templateUrl : '/pages/home.html',
 		controller : 'mainController'
 	})
 
 	//route for about page, this has its own controller
 	.when('/about', {
-		templateUrl : './pages/about.html',
+		templateUrl : '/pages/about.html',
 		controller : 'aboutController'
 	})
 
 	//route for contact page, this has its own controller
 	.when('/contact', {
-		templateUrl : './pages/contact.html',
+		templateUrl : '/pages/contact.html',
 		controller : 'contactController'
 	})
+	// .otherwise({
+	// 	redirectTo: '/pages/home.html'
+	// })
+
+	$locationProvider.html5Mode(true);
 });
 
 //created main controller and added the scope of the controller
-myPort.controller('mainController', function($scope){
+myPort.controller('mainController', function($scope) {
 	$scope.message = "This is the main page!!!"
 });
 
-myPort.controller('aboutController', function($scope){
+myPort.controller('aboutController', function($scope) {
 	$scope.message = "This is the about me page!!"
 });
 
-myPort.controller('contactController', function($scope){
+myPort.controller('contactController', function($scope) {
 	$scope.message = "This is the contact me page!!"
 });
--- server.js
+++ server.js
@@ -1,1 +1,10 @@
 //main server js file
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 3000
+
+app.use(express.static(__dirname + "/public"));
+
+app.listen(PORT, function() {
+  console.log("listening on port:" + PORT);
+});
